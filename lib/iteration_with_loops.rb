3fe8b2571f6b831da3bd1764460df744acc4e717
Victor Cordero <victordcordero@gmail.com>
2020-11-13 23:49:45 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,11 +5,14 @@ def join_nested_strings(src)
   # Combine all Strings present in the AoA into a single value and return it
   phrase = []
 # [
-    # ["The", "4", "quick"],
-    # [ "-1", "brown", "fox", "30", "studied"],
-    # ["101", "233", "Ruby"]
+    # ["The", 4, "quick"],
+    # [ -1, "brown", "fox", 30, "studied"],
+    # [101, 233, "Ruby"]
   # ]
-  
+  src.each do |sub_array|
+
+
+  end
   phrase
 end
 
