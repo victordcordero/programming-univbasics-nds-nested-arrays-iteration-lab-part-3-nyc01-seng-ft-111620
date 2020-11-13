2b6b5f9710ba80ff928e15b6155849c8852f8982
Victor Cordero <victordcordero@gmail.com>
2020-11-13 23:57:06 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -11,7 +11,7 @@ def join_nested_strings(src)
   # ]
   src.each do |sub_array|
     sub_array.each do |value|
-      if value.include == String
+      if value.include? == String
         push << value
   end
   phrase
