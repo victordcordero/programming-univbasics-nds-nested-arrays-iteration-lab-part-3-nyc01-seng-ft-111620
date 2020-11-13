e383c495a98561bd5caed14faa1af8d385f5d2c3
Victor Cordero <victordcordero@gmail.com>
2020-11-13 23:41:26 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -4,7 +4,6 @@ def join_nested_strings(src)
   # src will be an Array of Arrays of Strings and Integers
   # Combine all Strings present in the AoA into a single value and return it
   phrase = []
-  binding.pry
 
 
 end
